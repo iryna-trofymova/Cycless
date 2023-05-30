8b7bcf4a9b09366e3894c5fbbf2064890d110380
Iryna Trofymova <trofimovairena92@gmail.com>
2023-05-30 20:53:08 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+let input = +prompt("Enter a number of hours");
+
+while (!input || isNaN(input)) {
+  input = +prompt("Enter a NUMBER of hours");
+}
+
+let seconds = 3600 * input;
+alert("There are " + seconds + " seconds");
